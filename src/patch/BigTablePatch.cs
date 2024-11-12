d3b7602a7f61574534a22b282037a3e758063347
r0ute <route-a@ya.ru>
2024-11-12 00:40:02 -0600

    refactoring

--- src/patch/BigTablePatch.cs
+++ src/patch/BigTablePatch.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Devdog.InventoryPro;
 using HarmonyLib;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace D2E.src.patch
@@ -47,7 +48,7 @@ namespace D2E.src.patch
             itemTradeInfo2.m_CityID = cityid;
             itemTradeInfo2.m_Price = price;
             __instance.m_ItemBuyInfo[itemid].Add(itemTradeInfo2);
-            Plugin.Logger.LogDebug($"BigTable: AddItemBuyInfo item={itemid}, Count={__instance.m_ItemBuyInfo[itemid].Count}, Capacity={__instance.m_ItemBuyInfo[itemid].Capacity}");
+            Plugin.Logger.LogDebug($"BigTable: AddItemBuyInfo item={itemid}, Count={__instance.m_ItemBuyInfo[itemid].Count}");
 
         }
 
@@ -88,96 +89,35 @@ namespace D2E.src.patch
             itemTradeInfo2.m_CityID = cityid;
             itemTradeInfo2.m_Price = price;
             __instance.m_ItemSellInfo[itemid].Add(itemTradeInfo2);
-            Plugin.Logger.LogDebug($"BigTable: AddItemSellInfo item={itemid}, Count={__instance.m_ItemSellInfo[itemid].Count}, Capacity={__instance.m_ItemSellInfo[itemid].Capacity}");
-        }
-
-        [HarmonyPatch(typeof(BigTable), nameof(BigTable.Clear))]
-        [HarmonyPrefix]
-        static void OnClear(ref BigTable __instance)
-        {
-            Plugin.Logger.LogDebug($"BigTable: Clear");
-        }
-
-        [HarmonyPatch(typeof(InfoBoxUI), "BuyTip")]
-        [HarmonyPrefix]
-        static void OnBuyTip(ref InfoBoxUI __instance)
-        {
-            UnitySingleton<BigTable>.Instance.m_ItemBuyInfo.TryGetValue("ITC004001", out List<ItemTradeInfo> value);
-            Plugin.Logger.LogDebug($"InfoBoxUI: BuyTip Water Count {value.Count}");
-            Plugin.Logger.LogDebug($"InfoBoxUI: BuyTip Length {__instance.m_BuyInfoObj.Length}");
-        }
-
-        [HarmonyPatch(typeof(InfoBoxUI), "SellTip")]
-        [HarmonyPrefix]
-        static void OnSellTip(ref InfoBoxUI __instance)
-        {
-            UnitySingleton<BigTable>.Instance.m_ItemSellInfo.TryGetValue("ITC004001", out List<ItemTradeInfo> value);
-            Plugin.Logger.LogDebug($"InfoBoxUI: SellTip Water Count {value.Count}");
-            Plugin.Logger.LogDebug($"InfoBoxUI: SellTip Length {__instance.m_SellInfoObj.Length}");
+            Plugin.Logger.LogDebug($"BigTable: AddItemSellInfo item={itemid}, Count={__instance.m_ItemSellInfo[itemid].Count}");
         }
 
         [HarmonyPatch(typeof(InfoBoxUI), nameof(InfoBoxUI.AwakeFunction))]
         [HarmonyPrefix]
         static void BeforeAwake(ref InfoBoxUI __instance)
         {
-            Plugin.Logger.LogDebug($"BeforeAwake: m_BuyInfoObj Length {__instance.m_BuyInfoObj.Length}");
-            foreach (CommonObj obj in __instance.m_BuyInfoObj)
-            {
-                Plugin.Logger.LogDebug($"BeforeAwake: m_BuyInfoObj {obj.m_nID}");
-            }
-
-            Plugin.Logger.LogDebug($"BeforeAwake: m_SellInfoObj Length {__instance.m_SellInfoObj.Length}");
-            foreach (CommonObj obj in __instance.m_SellInfoObj)
-            {
-                Plugin.Logger.LogDebug($"BeforeAwake: m_SellInfoObj {obj.m_nID}");
-            }
-
-            if (__instance.m_BuyInfoObj.Length > 0)
-            {
-                Array.Resize(ref __instance.m_BuyInfoObj, Plugin.MaxTradeLogSize.Value);
-
-                if (__instance.m_BuyInfoObj.Length > 3)
-                {
-                    for (int ind = 3; ind < __instance.m_BuyInfoObj.Length; ind++)
-                    {
-                        __instance.m_BuyInfoObj[ind] = UnityEngine.Object.Instantiate(__instance.m_BuyInfoObj[0],
-                         __instance.m_BuyObj, false);
-                    }
-                }
-
-            }
-            Array.Resize(ref __instance.m_SellInfoObj, Plugin.MaxTradeLogSize.Value);
-            if (__instance.m_SellInfoObj.Length > 0)
-            {
-                Array.Resize(ref __instance.m_SellInfoObj, Plugin.MaxTradeLogSize.Value);
-
-                if (__instance.m_SellInfoObj.Length > 3)
-                {
-                    for (int ind = 3; ind < __instance.m_SellInfoObj.Length; ind++)
-                    {
-                        __instance.m_SellInfoObj[ind] = UnityEngine.Object.Instantiate(__instance.m_SellInfoObj[0],
-                        __instance.m_SellObj, false);
-                    }
-                }
-
-            }
+            ResizeTradeInfoObj(ref __instance.m_BuyInfoObj, __instance.m_BuyObj);
+            Plugin.Logger.LogDebug($"InfoBoxUI: AwakeFunction m_BuyInfoObj {__instance.m_BuyInfoObj.Length}");
+            ResizeTradeInfoObj(ref __instance.m_SellInfoObj, __instance.m_SellObj);
+            Plugin.Logger.LogDebug($"InfoBoxUI: AwakeFunction m_SellInfoObj {__instance.m_SellInfoObj.Length}");
         }
 
-        [HarmonyPatch(typeof(InfoBoxUI), nameof(InfoBoxUI.AwakeFunction))]
-        [HarmonyPostfix]
-        static void AfterAwake(ref InfoBoxUI __instance)
+        private static void ResizeTradeInfoObj(ref CommonObj[] tradeInfoObj, RectTransform parent)
         {
+            if (tradeInfoObj.Length > 0)
+            {
+                Array.Resize(ref tradeInfoObj, Plugin.MaxTradeLogSize.Value);
 
-            Plugin.Logger.LogDebug($"AfterAwake: m_BuyInfoObj Length {__instance.m_BuyInfoObj.Length}");
-            foreach (CommonObj obj in __instance.m_BuyInfoObj)
-            {
-                Plugin.Logger.LogDebug($"AfterAwake: m_BuyInfoObj {obj.m_nID}");
-            }
-            Plugin.Logger.LogDebug($"AfterAwake: m_SellInfoObj Length{__instance.m_SellInfoObj.Length}");
-            foreach (CommonObj obj in __instance.m_SellInfoObj)
-            {
-                Plugin.Logger.LogDebug($"AfterAwake: m_SellInfoObj {obj.m_nID}");
+                if (tradeInfoObj.Length > 3)
+                {
+                    for (int ind = 3; ind < tradeInfoObj.Length; ind++)
+                    {
+                        tradeInfoObj[ind] = UnityEngine.Object.Instantiate(tradeInfoObj[0], parent, false);
+                    }
+                }
+
             }
         }
+
     }
 }
