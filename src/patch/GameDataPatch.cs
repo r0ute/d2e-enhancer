404efc0c40977c0fbe45ad1bf5d7d1f5a780dab7
r0ute <route-a@ya.ru>
2024-11-14 02:35:35 -0600

    quick load on main screen

--- src/patch/GameDataPatch.cs
+++ src/patch/GameDataPatch.cs
@@ -6,15 +6,36 @@ namespace D2E.src.patch
     public class GameDataPatch
     {
 
+
         [HarmonyPatch(typeof(MainWindow), "CheckBtns")]
         [HarmonyPostfix]
         static void OnCheckBtns(ref MainWindow __instance, ref bool __runOriginal)
         {
+
+            HandleSaveGame();
+            HandleLoadGame();
+        }
+
+        [HarmonyPatch(typeof(PanelStart), "Update")]
+        [HarmonyPostfix]
+        static void OnStart(ref MainWindow __instance, ref bool __runOriginal)
+        {
+            HandleLoadGame();
+
+        }
+
+        private static void HandleSaveGame()
+        {
+
             if (Input.GetKeyDown(KeyCode.F5))
             {
                 Plugin.Logger.LogDebug($"MainWindow: GetKeyDown F5");
                 UnitySingleton<GameDataManager>.Instance.Save(3);
             }
+        }
+
+        private static void HandleLoadGame()
+        {
 
             if (Input.GetKeyDown(KeyCode.F9))
             {
