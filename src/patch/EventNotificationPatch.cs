a54a7d319abfead2e6a7353a2f3da71d6d5c4657
r0ute <route-a@ya.ru>
2024-11-14 20:54:18 -0600

    btn label

--- src/patch/EventNotificationPatch.cs
+++ src/patch/EventNotificationPatch.cs
@@ -7,22 +7,44 @@ namespace D2E.src.patch
 
         [HarmonyPatch(typeof(BattleManager), nameof(BattleManager.CheckLevelUp))]
         [HarmonyPrefix]
-        static void OnCheckLevelUp(ref BattleManager __instance, ref bool __runOriginal)
+        static void OnCheckLevelUp(ref bool __runOriginal)
         {
 
+            __runOriginal = ShowLevelUpNotification();
+
+        }
+
+
+        [HarmonyPatch(typeof(MainWindow), nameof(MainWindow.ActiveBtnLabel))]
+        [HarmonyPrefix]
+        static void OnActiveBtnLabel(int index, int type, ref MainWindow __instance, ref bool __runOriginal)
+        {
+
+            if (index == 1 && type == 1)
+            {
+                __runOriginal = ShowLevelUpNotification();
+            }
+        }
+
+        private static bool ShowLevelUpNotification()
+        {
             const int doctorMechanicTraderProfession = 3;
 
             foreach (int member in CharacterManager.Instance.m_MyTeam)
             {
-                if (CharacterManager.Instance.m_AllCharacters[member].m_Attr.m_nProfessionId < doctorMechanicTraderProfession)
+                if (CharacterManager.Instance.m_AllCharacters[member].m_Attr.CanUpLevel
+                && CharacterManager.Instance.m_AllCharacters[member].m_Attr.m_nProfessionId < doctorMechanicTraderProfession)
                 {
-                    __runOriginal = true;
-                    return;
+                    Plugin.Logger.LogDebug($"ShowLevelUpNotification member={CharacterManager.Instance.m_AllCharacters[member].m_Attr.m_PlayerName}");
+                    return true;
                 }
 
             }
 
-            __runOriginal = false;
+            return false;
+
         }
     }
+
+
 }
