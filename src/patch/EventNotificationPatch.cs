724bd75f91a907e8fb8962dfc21e8e69770f364c
r0ute <route-a@ya.ru>
2024-12-02 17:18:19 -0600

    equip bullet

--- src/patch/EventNotificationPatch.cs
+++ src/patch/EventNotificationPatch.cs
@@ -1,9 +1,41 @@
+using System.Collections.Generic;
+using Devdog.General;
+using Devdog.InventoryPro;
 using HarmonyLib;
 
 namespace D2E.src.patch
 {
     public class EventNotificationPatch
     {
+
+        [HarmonyPatch(typeof(BattleManager), nameof(BattleManager.CheckLevelUp))]
+        [HarmonyPrefix]
+        static void OnCheckLevelUp(ref BattleManager __instance, ref bool __runOriginal)
+        {
+
+            foreach (int memberId in CharacterManager.Instance.m_MyTeam)
+            {
+                SingleCharacter member = CharacterManager.Instance.m_AllCharacters[memberId]; ;
+                List<InventoryItemBase> list = ManagerBase<InventoryManager>.instance.Inventory.FindItemByType(ItemType.Type_Equipments, 4);
+                InventoryItemBase inventoryItemBase = member.m_Attr.Equipments[0];
+                int num = 0;
+                while (num < list.Count)
+                {
+                    if (inventoryItemBase.EquipConfig.BulletType != list[num].EquipConfig.BulletType)
+                    {
+                        list.RemoveAt(num);
+                    }
+                    else
+                    {
+                        num++;
+                    }
+                }
+
+                //todo: implement it
+
+            }
+        }
+
 
         [HarmonyPatch(typeof(BattleManager), nameof(BattleManager.CheckLevelUp))]
         [HarmonyPrefix]
